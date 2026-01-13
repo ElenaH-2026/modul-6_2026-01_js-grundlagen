0715cdc9e279dd7f26582264c1838de7f725405a
ElenaH-2026 <253562508+ElenaH-2026@users.noreply.github.com>
2026-01-13 20:29:46 +0100

    index.html and basics.js created

---
+++ basics.js
@@ -0,0 +1,45 @@
+/// Variablen (var, let, const)
+
+// "var" => war gestern -> nicht mehr verwenden!
+
+// "let" => veränderliche Variable
+let myAge = 33;
+console.log(myAge);
+
+myAge = 34;
+console.log(myAge);
+
+// "const" => konstante (UNveränderliche) Variable
+const myBirthyear = 1992;
+console.log(myBirthyear);
+
+
+/// Datentypen
+
+// "string" => Text in ""/''/`` (alles was in Anführungsstrichen steht)
+let myString = "http://127.0.0.1:5500/index.html";
+console.log(myString);
+
+// "number" => Zahl in JS (Ganz- und Kommazahl) funktioniert nicht in jeder Programiersprache
+let myNumber = 34.5265;
+console.log(myNumber);
+
+// "float" => Kommazahl
+let myFloat = 34.5265;
+console.log(myFloat);
+
+// "int" => Ganzzahl (Integer)
+let myInt = 34;
+console.log(myInt);
+
+// "boolean" => Wahrheitsaussage TRUE oder FALSE
+let myBoolean = true;
+console.log(myBoolean);
+
+// "array" => Liste in []
+let myArray = ["age", 34, "birthyear", 1992];
+console.log(myArray);
+
+// "object" => Liste in {key_1: value_1, key_2: value_2}
+let myObject = {"age": 34, "birthyear": 1992};
+console.log(myObject);
