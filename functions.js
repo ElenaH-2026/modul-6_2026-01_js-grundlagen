06b43af2291ef95103f0b0274666bd6920baa342
ElenaH-2026 <253562508+ElenaH-2026@users.noreply.github.com>
2026-01-14 14:24:13 +0100

    index.html modified, functions.js created

---
+++ functions.js
@@ -0,0 +1,46 @@
+/// Functions
+
+// Function mit globalen Variablen
+console.log("Function mit globalen Variablen");
+let discount = 50;
+let price = 500;
+
+console.log((price - discount) * 1.19)   // -> 535.5
+logCalculatePrice_globaleVariablen();   // -> 535.5 
+// -> !! Funktionen werden auch ausgeführt, wenn sie weiter unten im Code definiert wurden
+// -> !! Variablen können nur von oben nach unten berücksichtigt werden !!
+
+price = 400;
+
+function logCalculatePrice_globaleVariablen() {
+    console.log((price - discount) * 1.19);
+}
+
+logCalculatePrice_globaleVariablen();   // -> 416.5
+
+
+// Function mit Parametern
+console.log("Function mit Parametern");
+
+function logCalculatePrice_Parameter(discount, price, tax) {
+    console.log((price - discount) * tax);
+}
+
+logCalculatePrice_Parameter(20, 300, 1.19);   // -> 333.2
+
+
+// Function mit "return"
+console.log("Function mit return");
+
+function logCalculatePrice(discount, price, tax) {
+    let value = ((price - discount) * tax);
+    
+    return ("Calculated Price: " + value);
+}
+
+console.log(logCalculatePrice(10, 900, 1.19));
+
+
+// => Weiterführende Links:
+// Tutorial "JS Functions" von w3schools
+// https://www.w3schools.com/js/js_functions.asp
