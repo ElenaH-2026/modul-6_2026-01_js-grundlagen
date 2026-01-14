5e78c18223362f8e7f85c97ff612fa0e077187d3
ElenaH-2026 <253562508+ElenaH-2026@users.noreply.github.com>
2026-01-14 13:47:25 +0100

    conditional_code.js "if-else Test" added

--- conditional_code.js
+++ conditional_code.js
@@ -174,3 +174,78 @@ if(myIfCondition_2) {
     console.log("bye world!");   // -> bye world!
 }
 
+
+// Test => am 14.01.2026 alles korrekt gelöst :D
+console.log("Test if, else, else if")
+
+// Aufgabe 1:
+if (5 <= 10) {
+    console.log("a");
+}                       // -> a
+
+// Aufgabe 2:
+if (15 == 15) {
+    console.log("b");
+}                       // -> b
+
+// Aufgabe 3:
+if (20 > 10) {
+    console.log("c");
+}                       // -> c
+
+// Aufgabe 4:
+if (7 <= 7) {
+    console.log("a");
+} else {
+    console.log("b");
+}                       // -> a
+
+// Aufgabe 5:
+if (10 > 5) {
+    console.log("a");
+} else if (3 < 2) {
+    console.log("b");
+} else {
+    console.log("c");
+}                       // -> a
+
+// Aufgabe 6:
+if (12 >= 8 && 8 < 9) {
+    console.log("a");
+} else {
+    console.log("b");
+}                       // -> a
+
+// Aufgabe 7:
+if (3 == 3) {
+    console.log("a");
+} else if (10 > 20) {
+    console.log("b");
+} else {
+    console.log("c");
+}                       // -> a
+
+// Aufgabe 8:
+if (6 != 6) {
+    console.log("a");
+} else if (15 <= 20) {
+    console.log("b");
+} else {
+    console.log("c");
+}                       // -> b
+
+// Aufgabe 9:
+if (7 < 5) {
+    console.log("a");
+} else if (!(10 >= 10)) {
+    console.log("b");
+} else {
+    console.log("c");
+}                       // -> c
+
+// Aufgabe 10:
+if (4 >= 4 && 8 < 15 && 3 == 3) {
+    console.log("a");
+} else {
+    console.log("b");
+}                       // -> a
