6b2ad25ca8f8b9f6dd09d3e0828b29d49b1a295d
ElenaH-2026 <253562508+ElenaH-2026@users.noreply.github.com>
2026-01-15 19:58:46 +0100

    index.html and dom_manipulation.js added

--- dom_manipulation.js
+++ dom_manipulation.js
@@ -12,6 +12,22 @@ console.log(titel.innerHTML);   // -> Der Titel der Website
 
 titel.innerHTML = "neuer Titel";
 
-document.getElementById("test_div_1").innerHTML = "<p>test<p>";
-document.getElementById("test_div_2").innerText = "<p>test<p><script></script>";
-document.getElementById("test_div_3").innerHTML = "<p>test<p><script></script>";
+// .innerHTML vs. .innerText
+document.getElementById("test_div_1").innerHTML = "<p>test<p>";   // -> test
+document.getElementById("test_div_2").innerText = "<p>test<p><script></script>";   // -> <p>test<p><script></script>
+document.getElementById("test_div_3").innerHTML = "<p>test<p><script></script>";   // -> test
+
+// .classList (.add (Hinzugügen) / .remove (Entfernen) / .toggle (An-/Ausschalten abhängig von der Ausgangssituation))
+document.getElementById("test_div_1").classList.add("green_bg");
+document.getElementById("test_div_2").classList.remove("green_bg");
+document.getElementById("test_div_3").classList.toggle("green_bg");
+
+// Attribute setzten (verändern)
+document.getElementById("test_input_nr_1").setAttribute('type', 'text');
+document.getElementById("test_input_nr_1").setAttribute('value', 'text');
+document.getElementById("test_input_nr_2").value = 123;
+
+// Auslesen von Daten aus Input-Feldern
+console.log(document.getElementById("test_input_nr_0").value);   // -> 5
+console.log(document.getElementById("test_input_nr_1").value);   // -> text
+console.log(document.getElementById("test_input_nr_2").value);   // -> 123
