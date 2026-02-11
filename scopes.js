9322ac3557ae5e63fb8a4b17275b919ff7d2ff7c
ElenaH-2026 <253562508+ElenaH-2026@users.noreply.github.com>
2026-02-11 13:40:40 +0100

    index.html modified and scopes.js added

---
+++ scopes.js
@@ -0,0 +1,8 @@
+function scopeTest() {
+    let testScopeVar = 'hello world';
+    console.log(testScopeVar);
+    
+}
+
+// https://www.w3schools.com/js/js_scope.asp
+// https://developer.mozilla.org/en-US/docs/Glossary/Scope
