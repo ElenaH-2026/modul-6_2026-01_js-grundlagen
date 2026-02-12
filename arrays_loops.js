943e904f0245e34ca64663b24fefa3c659d73111
ElenaH-2026 <253562508+ElenaH-2026@users.noreply.github.com>
2026-02-12 19:12:01 +0100

    index.html Style added, arrays_loops.js new added

---
+++ arrays_loops.js
@@ -0,0 +1,15 @@
+/// Arrays und Schleifen
+console.log("/// Arrays und Schleifen");
+
+// https://www.w3schools.com/js/js_arrays.asp
+
+
+let myList = [34, 'Elena', true];
+
+function initArrays() {
+    console.log(myList);
+    console.log(myList[1]);
+    myList[1] += " Hiener"
+    console.log(myList[1]);
+    console.log(myList);
+}
